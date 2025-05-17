9105e82926181b4552212cb418efbc727044c537
Yasmine Lei De Sagun <yasmineleidsgn12@gmail.com>
2025-05-17 12:55:43 +0800

    feat: :sparkles: /chat integrated

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)  # allow React frontend to access this API
+
+@app.route('/chat', methods=['POST'])
+def chat():
+    data = request.get_json()
+    user_message = data.get('message', '').lower()
+
+    if user_message == 'hi':
+        reply = 'Good day, Yasmine! How can I help you?'
+    else:
+        reply = "I'm sorry, but I don't understand what you are trying to say."
+
+    return jsonify({'reply': reply})
 
 @app.route('/')
-def home():
-    return "Server is running!"
+def index():
+    return "Flask server is running!"
 
 if __name__ == '__main__':
     app.run(debug=True)
